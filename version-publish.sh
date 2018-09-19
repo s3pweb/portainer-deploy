ce8ee6b4d7d6b2a12f0464b56155540ab58ddcb6
Guillaume GABOREL <guillaume.gaborel@s3pweb.com>
2018-09-19 14:34:47 +0200

    fix: added (pipeline) to commit messages

--- version-publish.sh
+++ version-publish.sh
@@ -11,9 +11,9 @@ apt-get update && apt-get install -y jq;
 
 # Update minor version, tag and commit
 if [[ $1 = master ]]; then
-    npm version minor -m "ci: updated version to %s";
+    npm version minor -m "ci(pipeline): updated version to %s";
 else
-    npm version patch -m "ci: updated version to %s";
+    npm version patch -m "ci(pipeline): updated version to %s";
 fi;
 
 # Build and push to Docker hub
@@ -25,7 +25,7 @@ echo ${VERSION};
 
 sed s/:\\\${IMAGE_TAG}/:${VERSION}/g docker-compose-TEMPLATE.yaml > docker-compose.yaml;
 
-git commit -a -m "ci: updated docker-compose to version ${VERSION}";
+git commit -a -m "ci(pipeline): updated docker-compose to version ${VERSION}";
 
 git tag;
 git remote -v;
